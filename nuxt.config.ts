b500c568c62a20805d6dd84389f6e86c439e8e3d
kao <johnkao3@hotmail.com>
2024-06-24 16:07:56 +0800

    Add Package and .env.example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindTypography from "@tailwindcss/typography";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "dayjs-nuxt", "nuxt-gtag", "@nuxtjs/apollo"],
+  gtag: {
+    id: process.env.GTAG_ID,
+  },
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: "http://localhost:4000/graphql",
+        browserHttpEndpoint: "/graphql",
+      },
+    },
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: "cache-and-network",
+      },
+    },
+  },
+  vite: {
+    server: {
+      proxy: {
+        "/graphql": {
+          target: "http://localhost:4000",
+          changeOrigin: true,
+        },
+      },
+    },
+  },
+  tailwindcss: {
+    config: {
+      plugins: [tailwindTypography],
+    },
+  },
+});
